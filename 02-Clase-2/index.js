f0ada3758bf56454e3e8c0e5a9f218bd0554d68f
nicolasmisseri <nicomisseri1@gmail.com>
2022-09-11 19:17:10 -0300

    Desafio de la clase numero 6

--- 02-Clase-2/index.js
+++ 02-Clase-2/index.js
@@ -1,9 +1,45 @@
 const Container = require("./clase");
-const data = require("./archivo");
+// const data = require("./products");
+const data = require("./baseDeDatos/index")
 
-const products = new Container("./archivo.txt");
+const express = require('express')
+const moment  = require('moment')
+
+
+const products = new Container("./products.txt");
+console.log('PRODUCTS', products)
 products.save(data);
-products.getById(2);
-products.getAll();
-products.deleteById(2);
-products.deleteAll();
+// products.getById(2);
+// products.getAll();
+// products.deleteById(2);
+// products.deleteAll();
+
+const app = express()
+
+app.get('/', (req,res)=>{    
+    res.send(products)
+})
+
+app.get('/productos', (req,res)=>{    
+    res.send(data)
+})
+
+app.get('/productosRandom', (req,res)=>{
+    
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+
+    let valorRandom = getRandomInt(data.length)
+
+    res.send(data[valorRandom])
+    
+})
+
+const PORT = 8080;
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Listening on port ${server.address().port}`)
+})
+
+server.on('error', (error) => console.error(error))
