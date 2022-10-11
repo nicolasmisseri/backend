8cd71e355b9a39b831721b65dd665376059bb076
nicolasmisseri <nicomisseri1@gmail.com>
2022-10-10 22:07:22 -0300

    Websocket

--- 10-Clase-10/products-hbs.js
+++ 10-Clase-10/products-hbs.js
@@ -1,83 +1,89 @@
-import express from 'express';
+import express from "express";
+import { io } from "./server.js";
+
 const { Router } = express;
 const router = Router();
 
 let products = [];
 let productList = () => products;
 
-
-router.get('/', (req, res) => {
-     res.render('home');
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
-router.post('/', (req, res) => {
-    if (!products.length) {
-        products.push(Object.assign({}, req.body, { id: 1 }));
-    } else {
-        products.push(
-            Object.assign({}, req.body, {
-                id: products[products.length - 1].id + 1,
-            })
-        );
-    }
-    res.render('home', {
-        newProductSaved: true,
-        id: products[products.length - 1].id,
-        title: products[products.length - 1].title
-    });
-   
+router.post("/", (req, res) => {
+  if (!products.length) {
+    products.push(Object.assign({}, req.body, { id: 1 }));
+  } else {
+    products.push(
+      Object.assign({}, req.body, {
+        id: products[products.length - 1].id + 1,
+      })
+    );
+  }
+
+  // emitir producto nuevo
+
+  io.emit("new_product", products[products.length - 1]);
+
+  res.render("home", {
+    newProductSaved: true,
+    id: products[products.length - 1].id,
+    title: products[products.length - 1].title,
+  });
 });
 
-router.get('/productos', (req, res) => {
-    res.render('list', {
-        productsExist: products.length,
-        products: productList(),
-    });
-});
+router.get("/productos", (req, res) => {
+    io.on("new_products", (products) => res.render({products}))
+  res.render("list", {
+    productsExist: products.length,
+    products: productList(),
+  });
 
+});
 
 //otros metodos
-router.get('/:id', (req, res) => {
-    let productSelected = products.find(
-        (product) => product.id === parseInt(req.params.id)
-    );
-    if (!productSelected)
-        res.send(`There is no product with id ${req.params.id}`);
+router.get("/:id", (req, res) => {
+  let productSelected = products.find(
+    (product) => product.id === parseInt(req.params.id)
+  );
+  if (!productSelected)
+    res.send(`There is no product with id ${req.params.id}`);
 
-    res.send(productSelected);
+  res.send(productSelected);
 });
 
-router.put('/:id', (req, res) => {
-    let indexOfProductSelected = products.findIndex(
-        (product) => product.id === parseInt(req.params.id)
-    );
+router.put("/:id", (req, res) => {
+  let indexOfProductSelected = products.findIndex(
+    (product) => product.id === parseInt(req.params.id)
+  );
 
-    if (!indexOfProductSelected)
-        res.send(`There is no product with id ${req.params.id}`);
+  if (!indexOfProductSelected)
+    res.send(`There is no product with id ${req.params.id}`);
 
-    req.body.price
-        ? (products[indexOfProductSelected].price = req.body.price)
-        : null;
-    req.body.title
-        ? (products[indexOfProductSelected].title = req.body.title)
-        : null;
-    req.body.thumbnail
-        ? (products[indexOfProductSelected].thumbnail = req.body.thumbnail)
-        : null;
+  req.body.price
+    ? (products[indexOfProductSelected].price = req.body.price)
+    : null;
+  req.body.title
+    ? (products[indexOfProductSelected].title = req.body.title)
+    : null;
+  req.body.thumbnail
+    ? (products[indexOfProductSelected].thumbnail = req.body.thumbnail)
+    : null;
 
-    res.send(`Product with Id ${req.params.id} was updated`);
+  res.send(`Product with Id ${req.params.id} was updated`);
 });
 
-router.delete('/:id', (req, res) => {
-    let indexOfProductSelected = products.findIndex(
-        (product) => product.id === parseInt(req.params.id)
-    );
+router.delete("/:id", (req, res) => {
+  let indexOfProductSelected = products.findIndex(
+    (product) => product.id === parseInt(req.params.id)
+  );
 
-    if (!indexOfProductSelected)
-        res.send(`There is no product with id ${req.params.id}`);
+  if (!indexOfProductSelected)
+    res.send(`There is no product with id ${req.params.id}`);
 
-    products.splice(indexOfProductSelected, 1);
-    res.send(`Product with Id ${req.params.id} was deleted`);
+  products.splice(indexOfProductSelected, 1);
+  res.send(`Product with Id ${req.params.id} was deleted`);
 });
 
 export default router;
--- 10-Clase-10/server.js
+++ 10-Clase-10/server.js
@@ -1,36 +1,52 @@
-import express from 'express';
-import handlebars from 'express-handlebars'
-import productshbs from './products-hbs.js'
-import productsejs from './products-ejs.js'
-import productspug from './products-pug.js'
+import express from "express";
+import handlebars from "express-handlebars";
+import productshbs from "./products-hbs.js";
+import productsejs from "./products-ejs.js";
+import productspug from "./products-pug.js";
+import http from "http";
+import { Server } from "socket.io";
 
-
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
-const {json, urlencoded } = express;
-app.use(json())
-app.use(urlencoded({extended: true}));
-app.use(express.static('./public'));
+export const app = express();
+const { json, urlencoded } = express;
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(express.static("./public"));
 
 app.engine(
-    "hbs",
-    handlebars.engine({
-        extname: ".hbs" , 
-        defaultLayout: "index.hbs",
-        layoutsDir: __dirname +"/views/layouts",
-        partialsDir: __dirname +"/views/partials"
-    }) 
-)
-app.set("view engine", "hbs")
-app.set("views", "./views")
-app.use('/hbs/', productshbs);
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use("/hbs/", productshbs);
+app.use("/ejs/", productsejs);
+app.use("/pug/", productspug);
+
 const PORT = 8080;
-app.listen(PORT, (err)=>{
-    if(err) console.log(err);
-    console.log(`Listening on port ${PORT}`)
-})
+
+const server = http.createServer(app);
+
+export const io = new Server(server);
+io.on("connection", function (socket) {
+  console.log("Conecta2");
+});
+
+// app.listen(PORT, (err) => {
+//   if (err) console.log(err);
+//   console.log(`Listening on port ${PORT}`);
+// });
+
+server.listen(8080, () => {
+  console.log("listening on *:3000");
+});
